c8693835fc90b9ad67756b2523b499af51b550ef
Nicolas Demianiw <ndemia@fastmail.com>
2023-10-14 16:00:35 +0200

    Change favicon according to theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
@@ -7,6 +8,24 @@ import { GlobalStyles } from './theme/Global.styled';
 import DefaultTheme from './theme/default/DefaultTheme';
 
 export default function App() {
+	// Change favicon according to theme
+	const checkDarkMode = () => {
+		// In case browser's dark theme is enabled. Returns boolean
+		let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+		// Check for DOS mode as it has its own favicon
+		let isDos = document.documentElement.classList.contains('dos-theme');
+
+		// If dark true, and there's no DOS mode, show white favicon
+		if (isDark === true && isDos === false) {
+			document.querySelector('link[type="image/svg+xml"]').href = '/favicons/favicon-white.svg';
+		}
+	};
+
+	useEffect(() => {
+		checkDarkMode();
+	}, []);
+
 	return (
 		<DefaultTheme>
 			<>
