e2c7155cc9725b8a470a26b23e02efd914018ccf
Nicolas Demianiw <ndemia@fastmail.com>
2023-10-14 12:33:42 +0200

    Try new path for images

--- src/components/ProjectHeader/ProjectHeader.jsx
+++ src/components/ProjectHeader/ProjectHeader.jsx
@@ -3,11 +3,12 @@ import MainTitle from '../MainTitle/MainTitle';
 import InlineLink from '../InlineLink/InlineLink';
 
 export default function ProjectHeader({ project: { title, image_1x, image_2x, alt, github, demo } }) {
+	console.log(image_1x);
 	return (
 		<StyledProjectHeader>
 			<MainTitle title={title}></MainTitle>
 			<figure>
-				<img srcSet={`${image_1x} 700w, ${image_2x} 1280w`} sizes="(min-width: 960px) 1280px, 700px" src={image_2x} alt={alt} />
+				<img srcSet={`${image_1x} 700w, ${image_2x} 1280w`} sizes="(min-width: 960px) 1280px, 700px" src={image_1x} alt={alt} />
 			</figure>
 			<ul>
 				<li>
