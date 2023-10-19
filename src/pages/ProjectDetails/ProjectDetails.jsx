068eb8e7816382fdca21ce118b92bd468b9a29a4
Nicolas Demianiw <ndemia@fastmail.com>
2023-10-19 16:36:26 +0200

    Remove unnecessary fetch from Project Details component.

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import { useProjects } from '../../context/ProjectsContext';
 
 import { StyledLayoutContainer } from '../../components/LayoutContainer/LayoutContainer.styled';
 import Navigation from '../../components/Navigation/Navigation';
@@ -10,24 +11,18 @@ import Footer from '../../components/Footer/Footer';
 export default function ProjectDetails({ themeToggler }) {
 	const { id } = useParams();
 	const { state } = useLocation();
+	const [projects, setProjects] = useProjects();
 	const [project, setProject] = useState({});
 
-	const fetchProjects = async () => {
-		let response = await fetch('/data/projects.json');
-		let data = await response.json();
-		return data.projects;
-	};
-
 	useEffect(() => {
-		fetchProjects()
-			.then((projects) => {
-				let foundItem = projects.find((project) => project.id === id);
-				setProject(foundItem);
-				document.title = `${foundItem.title} - Nicolás Demianiw`;
-			})
-			.catch((error) => {
-				throw new Error(error.message);
-			});
+		// Filter projects to find the correct one to show.
+		let foundItem = projects.find((project) => project.id === id);
+
+		// Set it.
+		setProject(foundItem);
+
+		// Change HTML title.
+		document.title = `${foundItem.title} - Nicolás Demianiw`;
 		return () => {
 			document.title = `Home - Nicolás Demianiw`;
 		};
