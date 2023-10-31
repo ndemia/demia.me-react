f8c128a3378719f1793421f5f634760341880005
Nicolas Demianiw <ndemia@fastmail.com>
2023-10-31 10:16:59 +0100

    Change image size

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -7,7 +7,7 @@ export default function Image({ image_1x, image_2x, alt }) {
 				width={'700'}
 				height={'394'}
 				srcSet={`${image_1x} 700w, ${image_2x} 1280w`}
-				sizes="(min-width: 960px) 1280px, 700px"
+				sizes="(min-width: 1024px) 1280px, 700vw"
 				src={image_1x}
 				alt={alt}
 			/>
